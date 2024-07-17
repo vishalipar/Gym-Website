4d70d80b607d039e80eede70e7e9d25d0b361204
vishalipar <vishalipar04@gmail.com>
2024-07-17 10:37:54 +0530

    Built Signup, Login and Logout Page

--- gym/authgym/urls.py
+++ gym/authgym/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.handlelogin, name='handlelogin'),
+    path('logout/', views.handlelogout, name='handlelogout'),
 ]
